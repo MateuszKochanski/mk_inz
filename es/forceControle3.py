916e7b7834c06e55a7cfc4508b81593cdcab9a62
EasyRobots <matikoch2600@gmail.com>
2022-12-02 23:42:37 +0100

    dodano aktualny program z robota

---
+++ es/forceControle3.py
@@ -0,0 +1,58 @@
+import robotLib.es5 as es
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
+from geometry_msgs.msg import Pose
+import rospy
+from std_msgs.msg import String
+def point(name):
+    data = {}
+    return data[name]
+    
+def loadData(data):
+    var.x = data.pose.position.x
+    var.y = data.pose.position.y
+    var.z = data.pose.position.z
+    var.header = data.header
+    var.k = True
+    
+if (__name__ == '__main__'):
+    es.init()
+    var = es.Shared_variables()
+    main()
+    
+def callback(dane):
+    if (dane.header.seq == 0):
+        var.stamp = dane.header.stamp
+        loadData(dane)
+        
+    elif (dane.header.stamp > var.stamp):
+        loadData(dane)
+        var.stamp = dane.header.stamp
+        
+    
+def main():
+    var.k = False
+    var.x = 0.42
+    var.y = 0.22
+    var.z = 0.4
+    var.rx = -126.24999
+    var.ry = 1.60902
+    var.rz = -179.77854
+    pub = rospy.Publisher('done', Header, queue_size=10)
+    rospy.Subscriber('/sterowanie', PoseStamped, callback, queue_size=10)
+    es.move(type=es.Point, end=es.CartesianPos([var.x, var.y, var.z], [-126.24999, 1.60902, -179.77854], es.InternalPos([193.401, 89.597, 68.518, 110.514, 90.873, -130.335])), speed=20.0, acc=20.0)
+    es.execute_move()
+    while True:
+        #es.move(type=es.Point, end=es.CartesianPos([var.x, var.y, var.z], [-126.24999, 1.60902, -179.77854], es.InternalPos([193.401, 89.597, 68.518, 110.514, 90.873, -130.335])), speed=20.0, acc=20.0)
+        if (var.k == True):
+            var.header.frame_id = 'accepted'
+            pub.publish(var.header)
+            es.move(type=es.Point, end=es.CartesianPos([var.x, var.y, var.z], [var.rx, var.ry, var.rz], es.InternalPos([193.401, 89.597, 68.518, 110.514, 90.873, -130.335])), speed=20.0, acc=20.0)
+            es.execute_move()
+            var.header.frame_id = 'done'
+            pub.publish(var.header)
+            var.k = False
+            print(var.k)
+            
+        
+    
